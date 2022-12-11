e8d64288e63d2898df9246dcb57cff4586f56609
OWNER-PC\Owner <iwase2248@gmail.com>
2022-12-11 19:48:19 +0900

    masterコミット

---
+++ Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace Blanch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelEx5 = new Blanch.PanelEx();
+            this.panelEx6 = new Blanch.PanelEx();
+            this.panelEx1 = new Blanch.PanelEx();
+            this.panelEx2 = new Blanch.PanelEx();
+            this.panelEx3 = new Blanch.PanelEx();
+            this.panelEx4 = new Blanch.PanelEx();
+            this.panelEx7 = new Blanch.PanelEx();
+            this.panelEx8 = new Blanch.PanelEx();
+            this.panelEx9 = new Blanch.PanelEx();
+            this.panelEx10 = new Blanch.PanelEx();
+            this.panelEx11 = new Blanch.PanelEx();
+            this.panelEx12 = new Blanch.PanelEx();
+            this.panelEx13 = new Blanch.PanelEx();
+            this.panelEx14 = new Blanch.PanelEx();
+            this.panelEx15 = new Blanch.PanelEx();
+            this.panelEx16 = new Blanch.PanelEx();
+            this.panelEx17 = new Blanch.PanelEx();
+            this.panelEx18 = new Blanch.PanelEx();
+            this.panelEx19 = new Blanch.PanelEx();
+            this.panelEx20 = new Blanch.PanelEx();
+            this.panelEx5.SuspendLayout();
+            this.panelEx1.SuspendLayout();
+            this.panelEx3.SuspendLayout();
+            this.panelEx7.SuspendLayout();
+            this.panelEx9.SuspendLayout();
+            this.panelEx11.SuspendLayout();
+            this.panelEx13.SuspendLayout();
+            this.panelEx15.SuspendLayout();
+            this.panelEx17.SuspendLayout();
+            this.panelEx19.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelEx5
+            // 
+            this.panelEx5.AllowDrop = true;
+            this.panelEx5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx5.Controls.Add(this.panelEx6);
+            this.panelEx5.Location = new System.Drawing.Point(29, 66);
+            this.panelEx5.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.panelEx5.Name = "panelEx5";
+            this.panelEx5.Size = new System.Drawing.Size(80, 80);
+            this.panelEx5.TabIndex = 17;
+            // 
+            // panelEx6
+            // 
+            this.panelEx6.AllowDrop = true;
+            this.panelEx6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx6.Location = new System.Drawing.Point(-1, -1);
+            this.panelEx6.Name = "panelEx6";
+            this.panelEx6.Size = new System.Drawing.Size(73, 73);
+            this.panelEx6.TabIndex = 16;
+            // 
+            // panelEx1
+            // 
+            this.panelEx1.AllowDrop = true;
+            this.panelEx1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx1.Controls.Add(this.panelEx2);
+            this.panelEx1.Location = new System.Drawing.Point(149, 66);
+            this.panelEx1.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.panelEx1.Name = "panelEx1";
+            this.panelEx1.Size = new System.Drawing.Size(80, 80);
+            this.panelEx1.TabIndex = 18;
+            // 
+            // panelEx2
+            // 
+            this.panelEx2.AllowDrop = true;
+            this.panelEx2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx2.Location = new System.Drawing.Point(-1, -1);
+            this.panelEx2.Name = "panelEx2";
+            this.panelEx2.Size = new System.Drawing.Size(73, 73);
+            this.panelEx2.TabIndex = 16;
+            // 
+            // panelEx3
+            // 
+            this.panelEx3.AllowDrop = true;
+            this.panelEx3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx3.Controls.Add(this.panelEx4);
+            this.panelEx3.Location = new System.Drawing.Point(269, 66);
+            this.panelEx3.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.panelEx3.Name = "panelEx3";
+            this.panelEx3.Size = new System.Drawing.Size(80, 80);
+            this.panelEx3.TabIndex = 18;
+            // 
+            // panelEx4
+            // 
+            this.panelEx4.AllowDrop = true;
+            this.panelEx4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx4.Location = new System.Drawing.Point(-1, -1);
+            this.panelEx4.Name = "panelEx4";
+            this.panelEx4.Size = new System.Drawing.Size(73, 73);
+            this.panelEx4.TabIndex = 16;
+            // 
+            // panelEx7
+            // 
+            this.panelEx7.AllowDrop = true;
+            this.panelEx7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx7.Controls.Add(this.panelEx8);
+            this.panelEx7.Location = new System.Drawing.Point(389, 66);
+            this.panelEx7.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.panelEx7.Name = "panelEx7";
+            this.panelEx7.Size = new System.Drawing.Size(80, 80);
+            this.panelEx7.TabIndex = 18;
+            // 
+            // panelEx8
+            // 
+            this.panelEx8.AllowDrop = true;
+            this.panelEx8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx8.Location = new System.Drawing.Point(-1, -1);
+            this.panelEx8.Name = "panelEx8";
+            this.panelEx8.Size = new System.Drawing.Size(73, 73);
+            this.panelEx8.TabIndex = 16;
+            // 
+            // panelEx9
+            // 
+            this.panelEx9.AllowDrop = true;
+            this.panelEx9.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx9.Controls.Add(this.panelEx10);
+            this.panelEx9.Location = new System.Drawing.Point(509, 66);
+            this.panelEx9.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.panelEx9.Name = "panelEx9";
+            this.panelEx9.Size = new System.Drawing.Size(80, 80);
+            this.panelEx9.TabIndex = 18;
+            // 
+            // panelEx10
+            // 
+            this.panelEx10.AllowDrop = true;
+            this.panelEx10.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx10.Location = new System.Drawing.Point(-1, -1);
+            this.panelEx10.Name = "panelEx10";
+            this.panelEx10.Size = new System.Drawing.Size(73, 73);
+            this.panelEx10.TabIndex = 16;
+            // 
+            // panelEx11
+            // 
+            this.panelEx11.AllowDrop = true;
+            this.panelEx11.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx11.Controls.Add(this.panelEx12);
+            this.panelEx11.Location = new System.Drawing.Point(29, 188);
+            this.panelEx11.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.panelEx11.Name = "panelEx11";
+            this.panelEx11.Size = new System.Drawing.Size(80, 80);
+            this.panelEx11.TabIndex = 18;
+            // 
+            // panelEx12
+            // 
+            this.panelEx12.AllowDrop = true;
+            this.panelEx12.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx12.Location = new System.Drawing.Point(-1, -1);
+            this.panelEx12.Name = "panelEx12";
+            this.panelEx12.Size = new System.Drawing.Size(73, 73);
+            this.panelEx12.TabIndex = 16;
+            // 
+            // panelEx13
+            // 
+            this.panelEx13.AllowDrop = true;
+            this.panelEx13.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx13.Controls.Add(this.panelEx14);
+            this.panelEx13.Location = new System.Drawing.Point(149, 188);
+            this.panelEx13.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.panelEx13.Name = "panelEx13";
+            this.panelEx13.Size = new System.Drawing.Size(80, 80);
+            this.panelEx13.TabIndex = 18;
+            // 
+            // panelEx14
+            // 
+            this.panelEx14.AllowDrop = true;
+            this.panelEx14.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx14.Location = new System.Drawing.Point(-1, -1);
+            this.panelEx14.Name = "panelEx14";
+            this.panelEx14.Size = new System.Drawing.Size(73, 73);
+            this.panelEx14.TabIndex = 16;
+            // 
+            // panelEx15
+            // 
+            this.panelEx15.AllowDrop = true;
+            this.panelEx15.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx15.Controls.Add(this.panelEx16);
+            this.panelEx15.Location = new System.Drawing.Point(269, 188);
+            this.panelEx15.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.panelEx15.Name = "panelEx15";
+            this.panelEx15.Size = new System.Drawing.Size(80, 80);
+            this.panelEx15.TabIndex = 18;
+            // 
+            // panelEx16
+            // 
+            this.panelEx16.AllowDrop = true;
+            this.panelEx16.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx16.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx16.Location = new System.Drawing.Point(-1, -1);
+            this.panelEx16.Name = "panelEx16";
+            this.panelEx16.Size = new System.Drawing.Size(73, 73);
+            this.panelEx16.TabIndex = 16;
+            // 
+            // panelEx17
+            // 
+            this.panelEx17.AllowDrop = true;
+            this.panelEx17.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx17.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx17.Controls.Add(this.panelEx18);
+            this.panelEx17.Location = new System.Drawing.Point(389, 188);
+            this.panelEx17.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.panelEx17.Name = "panelEx17";
+            this.panelEx17.Size = new System.Drawing.Size(80, 80);
+            this.panelEx17.TabIndex = 18;
+            // 
+            // panelEx18
+            // 
+            this.panelEx18.AllowDrop = true;
+            this.panelEx18.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx18.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx18.Location = new System.Drawing.Point(-1, -1);
+            this.panelEx18.Name = "panelEx18";
+            this.panelEx18.Size = new System.Drawing.Size(73, 73);
+            this.panelEx18.TabIndex = 16;
+            // 
+            // panelEx19
+            // 
+            this.panelEx19.AllowDrop = true;
+            this.panelEx19.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx19.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx19.Controls.Add(this.panelEx20);
+            this.panelEx19.Location = new System.Drawing.Point(509, 188);
+            this.panelEx19.Margin = new System.Windows.Forms.Padding(20, 3, 20, 3);
+            this.panelEx19.Name = "panelEx19";
+            this.panelEx19.Size = new System.Drawing.Size(80, 80);
+            this.panelEx19.TabIndex = 18;
+            // 
+            // panelEx20
+            // 
+            this.panelEx20.AllowDrop = true;
+            this.panelEx20.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelEx20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEx20.Location = new System.Drawing.Point(-1, -1);
+            this.panelEx20.Name = "panelEx20";
+            this.panelEx20.Size = new System.Drawing.Size(73, 73);
+            this.panelEx20.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(621, 336);
+            this.Controls.Add(this.panelEx19);
+            this.Controls.Add(this.panelEx17);
+            this.Controls.Add(this.panelEx15);
+            this.Controls.Add(this.panelEx13);
+            this.Controls.Add(this.panelEx11);
+            this.Controls.Add(this.panelEx9);
+            this.Controls.Add(this.panelEx7);
+            this.Controls.Add(this.panelEx3);
+            this.Controls.Add(this.panelEx1);
+            this.Controls.Add(this.panelEx5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.panelEx5.ResumeLayout(false);
+            this.panelEx1.ResumeLayout(false);
+            this.panelEx3.ResumeLayout(false);
+            this.panelEx7.ResumeLayout(false);
+            this.panelEx9.ResumeLayout(false);
+            this.panelEx11.ResumeLayout(false);
+            this.panelEx13.ResumeLayout(false);
+            this.panelEx15.ResumeLayout(false);
+            this.panelEx17.ResumeLayout(false);
+            this.panelEx19.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private PanelEx panelEx5;
+        private PanelEx panelEx6;
+        private PanelEx panelEx1;
+        private PanelEx panelEx2;
+        private PanelEx panelEx3;
+        private PanelEx panelEx4;
+        private PanelEx panelEx7;
+        private PanelEx panelEx8;
+        private PanelEx panelEx9;
+        private PanelEx panelEx10;
+        private PanelEx panelEx11;
+        private PanelEx panelEx12;
+        private PanelEx panelEx13;
+        private PanelEx panelEx14;
+        private PanelEx panelEx15;
+        private PanelEx panelEx16;
+        private PanelEx panelEx17;
+        private PanelEx panelEx18;
+        private PanelEx panelEx19;
+        private PanelEx panelEx20;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blanch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+            //label1.BackColor = Color.Black;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelEx4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+
+    class PanelEx : Panel
+    {
+        string path = "";
+        PictureBox box = new PictureBox();
+
+        public PanelEx()
+        {
+            AllowDrop = true;
+            box.Parent = this;
+            box.Dock = DockStyle.Fill;
+            box.Click += Box_Click;
+            box.MouseMove += Box_MouseMove;
+        }
+
+        public string GetPath()
+        {
+            return path;
+        }
+
+        public void SetPath(string path0)
+        {
+            path = path0;
+
+            box.SizeMode = PictureBoxSizeMode.StretchImage;
+            if (File.Exists(path))
+            {
+                Icon appIcon = Icon.ExtractAssociatedIcon(path0);
+                box.Image = appIcon.ToBitmap();
+            }
+            else if (Directory.Exists(path0))
+            {
+                box.Image = GetFolderImage();
+            }
+        }
+
+        private void Box_MouseMove(object sender, MouseEventArgs e)
+        {
+            Form1 f = (Form1)FindForm();
+            //f.label1.ForeColor = Color.White;
+            //f.label1.Text = path;
+        }
+
+        private void Box_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Icon appIcon = Icon.ExtractAssociatedIcon(path);
+                System.Diagnostics.Process.Start(path);
+            }
+            catch
+            {
+                MessageBox.Show("ファイルが存在しません");
+            }
+        }
+
+        protected override void OnDragOver(DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        protected override void OnDragDrop(DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                path = files[0];
+
+                box.SizeMode = PictureBoxSizeMode.StretchImage;
+                if (File.Exists(path))
+                {
+                    Icon appIcon = Icon.ExtractAssociatedIcon(path);
+                    box.Image = appIcon.ToBitmap();
+                }
+                else if (Directory.Exists(path))
+                {
+                    box.Image = GetFolderImage();
+                }
+            }
+        }
+
+        Image GetFolderImage()
+        {
+            SHFILEINFO shinfo = new SHFILEINFO();
+            IntPtr hSuccess = SHGetFileInfo("", 0, ref shinfo, (uint)System.Runtime.InteropServices.Marshal.SizeOf(shinfo), SHGFI_ICON | SHGFI_LARGEICON);
+            if (hSuccess != IntPtr.Zero)
+            {
+                Icon appIcon = Icon.FromHandle(shinfo.hIcon);
+                return appIcon.ToBitmap();
+            }
+            return null;
+        }
+
+        // SHGetFileInfo関数
+        [System.Runtime.InteropServices.DllImport("shell32.dll")]
+        private static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, uint uFlags);
+
+        // SHGetFileInfo関数で使用する構造体
+        private struct SHFILEINFO
+        {
+            public IntPtr hIcon;
+            public IntPtr iIcon;
+            public uint dwAttributes;
+            [System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szDisplayName;
+            [System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 80)]
+            public string szTypeName;
+        };
+
+        // SHGetFileInfo関数で使用するフラグ
+        private const uint SHGFI_ICON = 0x100; // アイコン・リソースの取得
+        private const uint SHGFI_LARGEICON = 0x0; // 大きいアイコン
+        private const uint SHGFI_SMALLICON = 0x1; // 小さいアイコン
+    }
+}
